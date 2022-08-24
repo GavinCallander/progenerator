dce097303a2ea7264e3f53ec9b39f618b77a95e4
Gavin Callander <gavincallander@MacBook-Pro-2.local>
2022-08-24 13:21:32 -0400

    installing sass to make responsive design much easier

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -8,6 +8,5 @@ export default async function handler(req, res) {
     // read the json data file data.json
     const fileContents = await fs.readFile(jsonDir + '/projects.json', 'utf8');
     // return the content of the data file in json format
-    console.log(fileContents);
     res.status(200).send(fileContents);
 };
